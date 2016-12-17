e7c54afac63fa8ed7579d9c76b4978c912752dfd
DanielDublin <daniel_du@bialik.ort.org.il>
2016-12-17 17:36:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Client.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+    String dstAddress,result1;
+    int dstPort;
+    String response = "";
+    TextView textResponse;
+
+    Client(String addr, int port, TextView textResponse) {
+        dstAddress = addr;
+        dstPort = port;
+        this.textResponse = textResponse;
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(dstAddress, dstPort);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
+                    1024);
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            InputStream inputStream = socket.getInputStream();
+
+         /*
+          * notice: inputStream.read() will block if no data return
+          */
+            DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
+            DOS.writeUTF("HELLO_WORLD");
+
+
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "UnknownHostException: " + e.toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "IOException: " + e.toString();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+
+        textResponse.setText(result1);
+        super.onPostExecute(result);
+    }
+
+    public String getResult()
+    {
+        return result1;
+    }
+
+
+
+}
